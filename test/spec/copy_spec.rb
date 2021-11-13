13fccefaad255cacfaaf19580ef918974ceab0c7
Thomas Baron <tvbaron@gmail.com>
2021-11-13 16:44:22 +0100

    Change test spec

--- test/spec/copy_spec.rb
+++ test/spec/copy_spec.rb
@@ -84,3 +84,36 @@ describe 'copy JSON stdin to JSON file' do
     assert { File.read("#{get :tmp_dir}/out.json") == File.read("#{get :data_dir}/expect02.json") }
   end
 end
+
+describe 'copy JSON stdin to JSON stdout' do
+  before_all do
+    basedir = "#{get :basedir}"
+    set :data_dir, "#{basedir}/copy"
+    set :tmp_dir, "#{basedir}/tmp"
+    make_dir "#{get :tmp_dir}"
+  end
+
+  before_each do
+    clear_dir "#{get :tmp_dir}"
+  end
+
+  after_all do
+    clear_dir "#{get :tmp_dir}"
+  end
+
+  it 'copies (default)' do
+    si = StringIO.new
+    si.puts '{"name":"Althea"}'
+    so = StringIO.new
+    exec_prog ["--json", "-", "--copy", "--json", "-"], :stdin => si, :stdout => so
+    assert { so.string == File.read("#{get :data_dir}/expect01.json") }
+  end
+
+  it 'copies (pretty)' do
+    si = StringIO.new
+    si.puts '{"name":"Althea"}'
+    so = StringIO.new
+    exec_prog ["--json", "-", "--copy", "--json", "--pretty", "-"], :stdin => si, :stdout => so
+    assert { so.string == File.read("#{get :data_dir}/expect02.json") }
+  end
+end
