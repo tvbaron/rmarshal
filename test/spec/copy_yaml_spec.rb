17faaba517a1db62c6324e3d4f81c3436854751c
Thomas Baron <tvbaron@gmail.com>
2021-12-28 16:33:45 +0100

    Added test spec

--- test/spec/copy_yaml_spec.rb
+++ test/spec/copy_yaml_spec.rb
@@ -16,9 +16,48 @@ describe 'copy:yaml' do
       AppHelper.clear_dir(AppHelper.tmpdir)
     end
 
-    it 'copies (default)' do
+    it 'copies' do
       AppHelper.exec_prog ["#{context.datadir}/input01.yaml", "--copy", "#{AppHelper.tmpdir}/out.yaml"]
       expect(File.read("#{AppHelper.tmpdir}/out.yaml")).to eq(File.read("#{context.datadir}/expect01.yaml"))
+    end
+  end
+
+  describe 'copy YAML file to YAML stdout' do
+    before :each do
+      AppHelper.clear_dir(AppHelper.tmpdir)
+    end
+
+    it 'copies' do
+      sb = StringIO.new
+      AppHelper.exec_prog ["#{context.datadir}/input01.yaml", "--copy", "--yaml", "-"], :stdout => sb
+      expect(sb.string).to eq(File.read("#{context.datadir}/expect01.yaml"))
+    end
+  end
+
+  describe 'copy YAML stdin to YAML file' do
+    before :each do
+      AppHelper.clear_dir(AppHelper.tmpdir)
+    end
+
+    it 'copies' do
+      sb = StringIO.new
+      sb.puts '{"name":"Althea"}'
+      AppHelper.exec_prog ["--yaml", "-", "--copy", "#{AppHelper.tmpdir}/out.yaml"], :stdin => sb
+      expect(File.read("#{AppHelper.tmpdir}/out.yaml")).to eq(File.read("#{context.datadir}/expect01.yaml"))
+    end
+  end
+
+  describe 'copy YAML stdin to YAML stdout' do
+    before :each do
+      AppHelper.clear_dir(AppHelper.tmpdir)
+    end
+
+    it 'copies' do
+      si = StringIO.new
+      si.puts '{"name":"Althea"}'
+      so = StringIO.new
+      AppHelper.exec_prog ["--yaml", "-", "--copy", "--yaml", "-"], :stdin => si, :stdout => so
+      expect(so.string).to eq(File.read("#{context.datadir}/expect01.yaml"))
     end
   end
 end
