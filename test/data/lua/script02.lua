18bd94c60eb8d31718c98ec7605720cd8a5885bd
Thomas Baron <tvbaron@gmail.com>
2022-01-03 17:07:50 +0100

    Added test suite for Lua

---
+++ test/data/lua/script02.lua
@@ -0,0 +1,10 @@
+ctx:set_output(Object:new({
+    {"name", "Althea"},
+    {"score", 99},
+    {"elements", Array:new({true, 1, "hello"})},
+}))
+ctx:set_output(Object:new({
+    {"name", "Bernard"},
+    {"score", 98},
+    {"elements", Array:new({false, 2, "hey"})},
+}))
