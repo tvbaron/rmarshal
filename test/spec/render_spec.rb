730fa11e0b228b97b08e2d94ca9ff25b5174b7f2
Thomas Baron <tvbaron@gmail.com>
2022-01-07 17:12:20 +0100

    Added test cases

--- test/spec/render_spec.rb
+++ test/spec/render_spec.rb
@@ -29,8 +29,98 @@ describe 'render' do
 
     it 'renders' do
       sb = StringIO.new
-      AppHelper.exec_prog ["#{context.datadir}/input01.json", "--render", "#{context.datadir}/template01.txt", "--any", "-"], :stdout => sb
+      AppHelper.exec_prog ["#{context.datadir}/input01.json", "--render", "#{context.datadir}/template01.txt", "-"], :stdout => sb
       expect(sb.string).to eq(File.read("#{context.datadir}/expect01.txt"))
+    end
+  end
+
+  describe 'render a template with comment' do
+    before :each do
+      AppHelper.clear_dir(AppHelper.tmpdir)
+    end
+
+    it 'renders with trailing \'%>\'' do
+      expect = <<EOS
+===begin===
+
+===end===
+EOS
+      sb = StringIO.new
+      AppHelper.exec_prog ["#{context.datadir}/input01.json", "--render", "#{context.datadir}/template02.txt", "-"], :stdout => sb
+      expect(sb.string).to eq(expect)
+    end
+
+    it 'renders with trailing \'-%>\'' do
+      expect = <<EOS
+===begin===
+===end===
+EOS
+      sb = StringIO.new
+      AppHelper.exec_prog ["#{context.datadir}/input01.json", "--render", "#{context.datadir}/template03.txt", "-"], :stdout => sb
+      expect(sb.string).to eq(expect)
+    end
+  end
+
+  describe 'render a template with statement line' do
+    before :each do
+      AppHelper.clear_dir(AppHelper.tmpdir)
+    end
+
+    it 'renders with heading \'%\'' do
+      expect = <<EOS
+===begin===
+My name is Althea.
+And my score is 99.
+% Hey!
+===end===
+EOS
+      sb = StringIO.new
+      AppHelper.exec_prog ["#{context.datadir}/input01.json", "--render", "#{context.datadir}/template04.txt", "-"], :stdout => sb
+      expect(sb.string).to eq(expect)
+    end
+  end
+
+  describe 'render a template with statement' do
+    before :each do
+      AppHelper.clear_dir(AppHelper.tmpdir)
+    end
+
+    it 'renders with heading \'<%\'' do
+      expect = <<EOS
+===begin===
+  |
+My name is Althea.
+And my score is 99.
+===end===
+EOS
+      sb = StringIO.new
+      AppHelper.exec_prog ["#{context.datadir}/input01.json", "--render", "#{context.datadir}/template05.txt", "-"], :stdout => sb
+      expect(sb.string).to eq(expect)
+    end
+
+    it 'renders with heading \'<%-\'' do
+      expect = <<EOS
+===begin===
+|
+My name is Althea.
+And my score is 99.
+===end===
+EOS
+      sb = StringIO.new
+      AppHelper.exec_prog ["#{context.datadir}/input01.json", "--render", "#{context.datadir}/template06.txt", "-"], :stdout => sb
+      expect(sb.string).to eq(expect)
+    end
+
+    it 'renders with heading \'<%-\' and trailing \'-%>\'' do
+      expect = <<EOS
+===begin===
+My name is Althea.
+And my score is 99.
+===end===
+EOS
+      sb = StringIO.new
+      AppHelper.exec_prog ["#{context.datadir}/input01.json", "--render", "#{context.datadir}/template07.txt", "-"], :stdout => sb
+      expect(sb.string).to eq(expect)
     end
   end
 end
